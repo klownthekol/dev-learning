68d10e2f3e8b46027169f3b5567f85765e7a027e
klownthekol <klownthekol@gmail.com>
2025-12-07 15:32:14 -0400

    async testing

--- week1/variables.js
+++ week1/variables.js
@@ -1,4 +1,4 @@
-const name = 'Klown', age = 24;
+const myName = 'Klown', age = 24;
 let favouriteCoin = 'Solana', goalIncome = 10000;
 
-console.log(`Hello! My name is ${name} and i am ${age}, my favourite coin is ${favouriteCoin} and i want to make $${goalIncome}/month`);
+console.log(`Hello! My name is ${myName} and i am ${age}, my favourite coin is ${favouriteCoin} and i want to make $${goalIncome}/month`);
